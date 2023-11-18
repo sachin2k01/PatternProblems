0e2c0375b03edc832834db075ad8ec415529abda
sachin2k01 <thenamesachin@gmail.com>
2023-11-18 22:50:34 +0530

    1st corrected

--- src/Pattern1.java
+++ src/Pattern1.java
@@ -10,7 +10,7 @@ public class Pattern1
 
             for(int i=0; i<n;i++)
             {
-                for (int j=0;j<=i;j++)
+                for (int j=0;j<n;j++)
                 {
                     System.out.print("* ");
                 }
