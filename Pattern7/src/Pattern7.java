fe41b828144d7a0decb2a7514988099e18932a60
sachin2k01 <thenamesachin@gmail.com>
2023-11-20 19:04:29 +0530

    pattern 7 complete

---
+++ Pattern7/src/Pattern7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Pattern7
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter no of rows");
+        int r=sc.nextInt();
+        for (int i=1;i<=r;i++)
+        {
+            for (int k=1;k<=i;k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=r;j>=i;j--)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+    }
+}
