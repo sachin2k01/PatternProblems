c68ef66e19934c0643182dba7a91c57014b9fe53
sachin2k01 <thenamesachin@gmail.com>
2023-11-30 22:49:49 +0530

    pattern 9 is completed

--- Pattern9/src/Pattern9.java
+++ Pattern9/src/Pattern9.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
-public class Pattern8
+public class Pattern9
 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the Row Size");
-        int n=sc.nextInt();
-        for (int i=1;i<=n;i++) {
-            for (int j = 1; j <= n - i; j++)
+        System.out.println("Enter the row size");
+        int row=sc.nextInt();
+        for(int i=row;i>=1;i--)
+        {
+            for (int j=1;j<=row-i;j++)
             {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= 2 * i - 1; k++) {
+            for(int k=1;k<=2 * i- 1;k++)
+            {
                 System.out.print("*");
             }
             System.out.println();
